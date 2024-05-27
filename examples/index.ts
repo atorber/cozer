3db0bbe9ad6721e024e5b81e36081780e83c457e
LuChao <atorber@163.com>
2024-05-27 21:32:00 +0800

    0.2.0

--- examples/index.ts
+++ examples/index.ts
@@ -1,41 +1,3 @@
-# Cozer
-
-> 即将发布
-
-## 概述
-
-Cozer非官方coze SDK，基于coze API快速接入自己的业务系统
-
-- 获取bot列表
-- 获取bot详情
-- 启停bot
-- 添加bot
-- 对话bot
-- 查询bot对话列表
-- 查询bot对话历史
-- ...
-
-## 快速入门
-
-### 安装依赖
-
-```shell
-npm i cozer
-```
-
-### 配置环境变量
-
-在根目录创建一个名称为.env的配置文件
-
-```yaml
-COZE_ENDPOINT=https://api.coze.cn/open_api/v2/chat
-COZE_TOKEN='coze的API token'
-BOT_ID='coze的bot id'
-```
-
-### 示例代码
-
-```typescript
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 /* eslint-disable camelcase */
 /* eslint-disable sort-keys */
@@ -63,4 +25,4 @@ const answers = chatResp.extractAnswer()
 
 console.info('urls:', urls)
 console.info('answers:', answers)
-```
+
