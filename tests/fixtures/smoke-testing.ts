a9506ff4c5739add15c483a7d0d803f280f28d8f
LuChao <atorber@163.com>
2024-05-27 21:19:57 +0800

    Update smoke-testing.ts

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 import {
   VERSION,
-}                       from '../../src/mod.js'
+}                       from 'cozer'
 
 async function main () {
   if (VERSION === '0.0.0') {
